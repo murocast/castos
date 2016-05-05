9b685657d7dfe05435ff2e846d9a3ec18438b98f
Sebastian Brandt <bastibrandt@gmail.com>
2016-05-05 16:34:07 +0200

    reads episodes

--- src/Castos.Api/FileSystem.fs
+++ src/Castos.Api/FileSystem.fs
@@ -6,14 +6,22 @@ open System.IO
 module FileSystem =
     let path = @"\\qnap\Music\Podcasts"
 
+    let episodes path =
+        let files = Directory.GetFiles(path)
+                    |> Seq.map(fun x -> {Name = Path.GetFileName(x)
+                                         Length = TimeSpan.FromMinutes(0.) //TODO: Read from File
+                                         Path = x})
+                    |> Seq.toList
+        files
+
     let podcastsOfCategory path category =
-        let categoryPath = path + "//" + category
+        let categoryPath = path + "\\" + category
         let podcasts = Directory.GetDirectories(categoryPath)
                        |> Seq.map(fun x -> { Name = x.Substring(categoryPath.Length + 1)
                                              Category = category
                                              Folder = x
                                              Current = None
-                                             Episodes = [] })
+                                             Episodes = episodes x })
                        |> Seq.toList
         podcasts
 
