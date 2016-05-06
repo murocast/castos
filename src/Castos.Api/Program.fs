0cf5526178dcc9a5780f27720d7b559cfb69798d
Sebastian Brandt <bastibrandt@gmail.com>
2016-05-06 22:11:11 +0200

    uses podcast routes

--- src/Castos.Api/Program.fs
+++ src/Castos.Api/Program.fs
@@ -4,16 +4,21 @@ open Suave.Operators
 open Suave.Successful
 open Suave.WebPart
 
-let podcastRoutes = choose [ path     "/api/podcasts" >=> choose [
-                                GET >=> OK "TODO: All podcasts" ]
-                             pathScan "/api/podcasts/%s" <| fun podcast -> choose [
-                                GET >=> OK (sprintf "TODO: Show information about podcast '%s'" podcast) ]
-                             pathScan "/api/podcasts/%s/episodes" <| fun podcast -> choose [
-                                GET >=> OK (sprintf "TODO: Show episodes of '%s'" podcast) ]
-                             pathScan "/api/podcasts/%s/episodes/%s" <| fun (podcast, episode) -> choose [
-                                GET >=> OK (sprintf "TODO: Show information about episode '%s' of podcast '%s'" podcast episode)]]
+let podcastRoutes =
+    choose
+        [ path "/api/podcasts" >=> choose [ GET >=> OK "TODO: All podcasts" ]
+
+          pathScan "/api/podcasts/%s"
+          <| fun podcast -> choose [ GET >=> OK(sprintf "TODO: Show information about podcast '%s'" podcast) ]
+
+          pathScan "/api/podcasts/%s/episodes"
+          <| fun podcast -> choose [ GET >=> OK(sprintf "TODO: Show episodes of '%s'" podcast) ]
+
+          pathScan "/api/podcasts/%s/episodes/%s"
+          <| fun (podcast, episode) ->
+              choose [ GET >=> OK(sprintf "TODO: Show information about episode '%s' of podcast '%s'" podcast episode) ] ]
 
 [<EntryPoint>]
 let main argv =
-    startWebServer defaultConfig (OK "Hello World!")
+    startWebServer defaultConfig (choose [ podcastRoutes ])
     0
