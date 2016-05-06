314bbb46c67c5f6cad649aed075bb8b52c8b951f
Sebastian Brandt <bastibrandt@gmail.com>
2016-05-06 21:50:42 +0200

    defined routes for podcasts

--- src/Castos.Api/Program.fs
+++ src/Castos.Api/Program.fs
@@ -1,5 +1,17 @@
 ﻿open Suave
+open Suave.Filters
+open Suave.Operators
 open Suave.Successful
+open Suave.WebPart
+
+let podcastRoutes = choose [ path     "/api/podcasts" >=> choose [
+                                GET >=> OK "TODO: All podcasts" ]
+                             pathScan "/api/podcasts/%s" <| fun podcast -> choose [
+                                GET >=> OK (sprintf "TODO: Show information about podcast '%s'" podcast) ]
+                             pathScan "/api/podcasts/%s/episodes" <| fun podcast -> choose [
+                                GET >=> OK (sprintf "TODO: Show episodes of '%s'" podcast) ]
+                             pathScan "/api/podcasts/%s/episodes/%s" <| fun (podcast, episode) -> choose [
+                                GET >=> OK (sprintf "TODO: Show information about episode '%s' of podcast '%s'" podcast episode)]]
 
 [<EntryPoint>]
 let main argv =
