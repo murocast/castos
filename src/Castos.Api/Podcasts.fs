14d8889278950d2eaa19039889cd43134e1a22f3
Sebastian Brandt <bastibrandt@gmail.com>
2016-05-16 11:23:53 +0200

    merged filesystem.fs and podcasts.fs

--- src/Castos.Api/Podcasts.fs
+++ src/Castos.Api/Podcasts.fs
@@ -1,8 +1,8 @@
 ﻿namespace Castos
 
 open System
+open System.IO
 
-[<AutoOpen>]
 module Podcasts =
     type Episode =
         { Name : string
@@ -19,3 +19,33 @@ module Podcasts =
           Category : string
           Current : PendingEpisode option
           Episodes : Episode list }
+
+    let basePath = @"\\qnap\Music\Podcasts"
+
+    let episodes path =
+        let files = Directory.GetFiles(path)
+                    |> Seq.map(fun x -> {Name = Path.GetFileName(x)
+                                         Length = TimeSpan.FromMinutes(0.) //TODO: Read from File
+                                         Path = x})
+                    |> Seq.toList
+        files
+
+    let podcastsOfCategory path category =
+        let categoryPath = path + "\\" + category
+        let podcasts = Directory.GetDirectories(categoryPath)
+                       |> Seq.map(fun x -> { Name = x.Substring(categoryPath.Length + 1)
+                                             Category = category
+                                             Folder = x
+                                             Current = None
+                                             Episodes = episodes x })
+                       |> Seq.toList
+        podcasts
+
+    let Podcasts =
+        let categories = Directory.GetDirectories(basePath)
+                         |> Seq.filter(fun x -> not(x.Substring(basePath.Length + 1).StartsWith(".")))
+                         |> Seq.map(fun x -> x.Substring(basePath.Length + 1))
+                         |> Seq.toList
+        categories
+        |> Seq.collect(fun x -> podcastsOfCategory basePath x)
+        |> Seq.toList
