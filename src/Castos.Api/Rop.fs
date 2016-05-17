5e1c32f11d3108b42f06e4fd4f058bd8bcf67e2f
Sebastian Brandt <bastibrandt@gmail.com>
2016-05-17 22:31:38 +0200

    added simple version of rop

---
+++ src/Castos.Api/Rop.fs
@@ -0,0 +1,15 @@
+﻿[<AutoOpen>]
+module Rop
+    type Result<'TSuccess, 'TFailure> =
+        | Success of 'TSuccess
+        | Failure of 'TFailure
+
+    let bind f x =
+        match x with
+        | Success s -> f s
+        | Failure f -> Failure f
+
+    let map f x =
+        match x with
+        | Success s -> Success(f s)
+        | Failure f -> Failure f
