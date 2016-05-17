e69bbc61d7a28a5b1084a1fcb798bb0200d5ddc6
Sebastian Brandt <bastibrandt@gmail.com>
2016-05-17 22:05:29 +0200

    using Path.Combine instead of string concat

--- src/Castos.Api/Podcasts.fs
+++ src/Castos.Api/Podcasts.fs
@@ -23,29 +23,35 @@ module Podcasts =
     let basePath = @"\\qnap\Music\Podcasts"
 
     let episodes path =
-        let files = Directory.GetFiles(path)
-                    |> Seq.map(fun x -> {Name = Path.GetFileName(x)
-                                         Length = TimeSpan.FromMinutes(0.) //TODO: Read from File
-                                         Path = x})
-                    |> Seq.toList
+        let files =
+            Directory.GetFiles(path)
+            |> Seq.map (fun x ->
+                   { Name = Path.GetFileName(x)
+                     Length = TimeSpan.FromMinutes(0.) //TODO: Read from File
+                     Path = x })
+            |> Seq.toList
         files
 
     let podcastsOfCategory path category =
-        let categoryPath = path + "\\" + category
-        let podcasts = Directory.GetDirectories(categoryPath)
-                       |> Seq.map(fun x -> { Name = x.Substring(categoryPath.Length + 1)
-                                             Category = category
-                                             Folder = x
-                                             Current = None
-                                             Episodes = episodes x })
-                       |> Seq.toList
+        let categoryPath = Path.Combine(path, category)
+
+        let podcasts =
+            Directory.GetDirectories(categoryPath)
+            |> Seq.map (fun x ->
+                   { Name = x.Substring(categoryPath.Length + 1)
+                     Category = category
+                     Folder = x
+                     Current = None
+                     Episodes = episodes x })
+            |> Seq.toList
         podcasts
 
     let Podcasts =
-        let categories = Directory.GetDirectories(basePath)
-                         |> Seq.filter(fun x -> not(x.Substring(basePath.Length + 1).StartsWith(".")))
-                         |> Seq.map(fun x -> x.Substring(basePath.Length + 1))
-                         |> Seq.toList
+        let categories =
+            Directory.GetDirectories(basePath)
+            |> Seq.filter (fun x -> not (x.Substring(basePath.Length + 1).StartsWith(".")))
+            |> Seq.map (fun x -> x.Substring(basePath.Length + 1))
+            |> Seq.toList
         categories
-        |> Seq.collect(fun x -> podcastsOfCategory basePath x)
+        |> Seq.collect (fun x -> podcastsOfCategory basePath x)
         |> Seq.toList
