85772b2390e3633f0e556b57cb18a0affccff54d
Sebastian Brandt <bastibrandt@gmail.com>
2016-11-02 20:04:36 +0100

    added xml type provider for getMetadataRequest

--- src/Castos.Api/Smapi.fs
+++ src/Castos.Api/Smapi.fs
@@ -1,10 +1,15 @@
 ﻿namespace Castos
 
+open FSharp.Data
+
 module Smapi =
+    type getMetadataRequest = XmlProvider<"Samples/GetMetadataRequest.xml">
+
     let extractSmapiMethod (m:string) =
         m.[34..] //cut until #: http://www.sonos.com/Services/1.1#getMetadata
 
     let processGetMetadata s =
+        let req = getMetadataRequest.Parse s
         Success(s)
 
     let processSmapiMethod a form =
