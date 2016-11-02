ffcfbe3137ed73156eea2c55c1db978b794d3ce7
Sebastian Brandt <bastibrandt@gmail.com>
2016-11-02 22:21:16 +0100

    returns now the root collection

--- src/Castos.Api/Smapi.fs
+++ src/Castos.Api/Smapi.fs
@@ -10,19 +10,26 @@ module Smapi =
     let extractSmapiMethod (m:string) =
         m.[34..] //cut until #: http://www.sonos.com/Services/1.1#getMetadata
 
+    let getRootCollections =
+        [ MediaCollection { Id = "library"
+                            ItemType = Collection
+                            Title = "Library"
+                            CanPlay = false }
+          MediaCollection { Id = "current"
+                            ItemType = Collection
+                            Title = "Current"
+                            CanPlay = false }
+          MediaCollection { Id = "recent"
+                            ItemType = Collection
+                            Title = "Recent"
+                            CanPlay = false }]
+
     let processGetMetadata s =
         let req = getMetadataRequest.Parse s
-        let items = [ MediaMetadata { Id = ""
-                                      ItemType = Artist
-                                      Title = "TitleOne"
-                                      MimeType = "media/mp3"
-                                      ItemMetadata = TrackMetadata { AlbumId = "Album1"
-                                                                     Duration = 123
-                                                                     ArtistId = "Artist1"
-                                                                     Genre = ""
-                                                                     Artist = "Mobi"
-                                                                     Album = "First"
-                                                                     AlbumArtURI = "" }}]
+        let items = match req.Body.GetMetadata.Id with
+                    | "root" -> getRootCollections
+                    | _ -> failwith "unknown id"
+
         let response = getMetadataResponse items
         Success(response)
 
