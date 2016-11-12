48119d74ecd29f2f532424c065ac33c9691775ec
Sebastian Brandt <bastibrandt@gmail.com>
2016-11-12 21:03:16 +0100

    Stub for getMediaMetadata

--- src/Castos.Api/Smapi.fs
+++ src/Castos.Api/Smapi.fs
@@ -5,21 +5,31 @@ open Smapi.Respond
 open FSharp.Data
 
 module Smapi =
+    [<Literal>]
+    let RootId = "root"
+    [<Literal>]
+    let LibraryId = "library"
+    [<Literal>]
+    let CurrentId = "current"
+    [<Literal>]
+    let RecentId = "recent"
+
     type getMetadataRequest = XmlProvider<"Samples/GetMetadataRequest.xml">
+    type getMediaMetadataRequest = XmlProvider<"Samples/GetMediaMetadataRequest.xml">
 
     let extractSmapiMethod (m:string) =
         m.[34..] //cut until #: http://www.sonos.com/Services/1.1#getMetadata
 
     let getRootCollections =
-        [ MediaCollection { Id = "library"
+        [ MediaCollection { Id = LibraryId
                             ItemType = Collection
                             Title = "Library"
                             CanPlay = false }
-          MediaCollection { Id = "current"
+          MediaCollection { Id = CurrentId
                             ItemType = Collection
                             Title = "Current"
                             CanPlay = false }
-          MediaCollection { Id = "recent"
+          MediaCollection { Id = RecentId
                             ItemType = Collection
                             Title = "Recent"
                             CanPlay = false }]
@@ -27,11 +37,20 @@ module Smapi =
     let processGetMetadata s =
         let req = getMetadataRequest.Parse s
         let items = match req.Body.GetMetadata.Id with
-                    | "root" -> getRootCollections
+                    | RootId -> getRootCollections
+                    | LibraryId
+                    | CurrentId
+                    | RecentId
                     | _ -> failwith "unknown id"
 
         let response = getMetadataResponse items
         Success(response)
+
+    let processGetMediaMetadata s =
+        let req = getMediaMetadataRequest.Parse s
+//        let items = match req.Body.GetMediaMetadata.Id with
+//                    | RootId ->
+        failwith "TODO"
 
     let processSmapiMethod a form =
         match extractSmapiMethod a with
