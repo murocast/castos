c358f6175be8593c072963911b0e1512101509e3
Sebastian Brandt <bastibrandt@gmail.com>
2017-02-12 21:51:46 +0100

    switched to expecto

--- tests/Smapi.Tests/Smapi.Tests.fs
+++ tests/Smapi.Tests/Smapi.Tests.fs
@@ -1,8 +1,6 @@
 module Smapi.Tests
 
 open Smapi.Respond
-open NUnit.Framework
-open FsUnit
 
 open System
 open System.IO
@@ -11,25 +9,33 @@ open System.Xml
 open System.Xml.XPath
 open System.Xml.Linq
 
-[<Test>]
-let ``Count should return number of items in collection`` () =
-    let items = [ MediaMetadata {  Id = ""
-                                   ItemType = Artist
-                                   Title = "TitleOne"
-                                   MimeType = "media/mp3"
-                                   ItemMetadata = TrackMetadata { AlbumId = "Album1"
-                                                                  Duration = 123
-                                                                  ArtistId = "Artist1"
-                                                                  Genre = ""
-                                                                  Artist = "Mobi"
-                                                                  Album = "First"
-                                                                  AlbumArtURI = "" }}]
+open Expecto
 
-    let metadata = getMetadataResponse (Seq.ofList items)
-    let reader = XmlReader.Create(new StringReader(metadata))
-    let xml = XElement.Load(reader)
-    let nameTable = reader.NameTable
-    let nsManager = new XmlNamespaceManager(nameTable)
-    nsManager.AddNamespace("sn", NsSonos)
-    let count = xml.XPathSelectElements("//sn:count", nsManager).Single()
-    count.Value |> should equal "1"
+[<Tests>]
+let smapi = 
+    testList "samples" [
+        testCase "Count should return number of items in collection" <| fun _ ->
+            let items = [ MediaMetadata {  Id = ""
+                                           ItemType = Artist
+                                           Title = "TitleOne"
+                                           MimeType = "media/mp3"
+                                           ItemMetadata = TrackMetadata { AlbumId = "Album1"
+                                                                          Duration = 123
+                                                                          ArtistId = "Artist1"
+                                                                          Genre = ""
+                                                                          Artist = "Mobi"
+                                                                          Album = "First"
+                                                                          AlbumArtURI = "" }}]
+            let metadata = getMetadataResponse (Seq.ofList items)
+            let reader = XmlReader.Create(new StringReader(metadata))
+            let xml = XElement.Load(reader)
+            let nameTable = reader.NameTable
+            let nsManager = new XmlNamespaceManager(nameTable)
+            nsManager.AddNamespace("sn", NsSonos)
+            let count = xml.XPathSelectElements("//sn:count", nsManager).Single()
+            Expect.equal "1" count.Value "XML-Node should be there"
+    ]
+
+[<EntryPoint>]
+let main args =
+  runTestsInAssembly defaultConfig args
