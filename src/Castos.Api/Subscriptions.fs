fc70c830879b9983bebf5047c2ba2bc60a29bdc0
Sebastian Brandt <bastibrandt@gmail.com>
2017-07-22 21:39:55 +0200

    evolve and apply for subscription events

--- src/Castos.Api/Subscriptions.fs
+++ src/Castos.Api/Subscriptions.fs
@@ -8,8 +8,27 @@ type Subscription = {
 }
 
 module SubscriptionSource =
+    let private initialSubscriptionState =
+        { Id = System.Guid.Empty
+          Url = ""
+          Name = ""
+          Active = false }
+          
+    let private apply state event =
+        match event with
+        | SubscriptionAdded ev -> { state with Id = ev.Id
+                                               Url = ev.Url
+                                               Name = ev.Name
+                                               Active = true }
+        | SubscriptionDeleted ev -> { state with Active = false }
 
-    let addSubscription name url =
+    let rec private evolve state events =
+        match events with
+        | head :: rest -> evolve (apply state head) rest
+        | [] -> state
+
+    let addSubscription name url =        
         SubscriptionAdded { Id = System.Guid.NewGuid()
                             Name = name
                             Url = url }
+
