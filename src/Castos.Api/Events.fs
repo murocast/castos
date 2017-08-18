566e84483ae2d504ca07d7c92ad9b0a7749a06c6
Sebastian Brandt <bastibrandt@gmail.com>
2017-08-18 20:48:23 +0200

    planned events in two stream categories

--- src/Castos.Api/Events.fs
+++ src/Castos.Api/Events.fs
@@ -4,23 +4,50 @@ module Castos.Events
 open Castos.Podcasts
 
 type CastosEventData =
-    | PlaySecondsReported of PlaySecondsReported
-    | PlayEpisodeStopped of PlayEpisodeStopped
-    
+    //Stream per subscription
     | SubscriptionAdded of SubscriptionAdded
     | SubscriptionDeleted of SubscriptionDeleted
-and PlaySecondsReported = {
-        Id: PodcastId
-        Position: int }
-and PlayEpisodeStopped = {
-        Id: PodcastId
-        Position: int }
+    | EpisodeAdded of EpisodeAdded
+
+    //Stream per user
+    | Subscribed
+    | SubscriptionCanceled
+    | PlayEpisodeStarted of PlayEpisodeStarted
+    | PlayEpisodeStopped of PlayEpisodeStopped
+    | PlaySecondsReported of PlaySecondsReported
+
 and SubscriptionAdded = {
     Id: SubscriptionId
-    Url: string 
+    Url: string
     Name: string }
 and SubscriptionDeleted = { Id: SubscriptionId }
+and EpisodeAdded = {
+    Id: EpisodeId
+    SubscriptionId: SubscriptionId
+    MediaUrl: string
+    Title: string
+    ReleaseDate: System.DateTime
+}
+and PlayEpisodeStarted = {
+    Id: EpisodeId
+    TimeStamp: System.DateTime
+    Position: int
+    Player: string
+}
+and PlaySecondsReported = {
+    Id: PodcastId //TODO: EpisodeId
+    Position: int }
+and PlayEpisodeStopped = {
+    Id: PodcastId //TODO: EpisodeId
+    Position: int }
+and Subscribed = {
+    SubscriptionId: SubscriptionId
+}
+and SubscriptionCanceled = {
+    SubscriptionId: SubscriptionId
+}
 and SubscriptionId = System.Guid
+and EpisodeId = int
 
 type Error =
     | NotImplemented of string
