5e24ea4e2d7df4b609b2358ddb0ddf007290b10d
Sebastian Brandt <bastibrandt@gmail.com>
2019-03-24 13:59:36 +0100

    added token endpoint

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -1,20 +1,65 @@
-open System.IO
+module Server
 
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Giraffe
 open Giraffe.Serialization
+open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.IdentityModel.Tokens
 open Saturn
+open Saturn.CSRF.View
+open System
+open System.IdentityModel.Tokens.Jwt
+open System.IO
+open System.Security.Claims
 
 open Castos.EventStore
 open Castos
 open Castos.SmapiCompositions
 open Castos.SubscriptionCompositions
 
+let secret = "spadR2dre#u-ruBrE@TepA&*Uf@U"
+let issuer = "saturnframework.io"
+
 let publicPath = Path.GetFullPath "../Client/public"
 let port = 80us
 
 let eventStore = createGetEventStoreEventStore<CastosEventData, Error>(VersionConflict "Version conflict")
 
+[<CLIMutable>]
+type LoginViewModel =
+    {
+        Email : string
+        Password : string
+    }
+
+[<CLIMutable>]
+type TokenResult =
+    {
+        Token : string
+    }
+
+let generateToken email =
+    let claims = [|
+        Claim(JwtRegisteredClaimNames.Sub, email);
+        Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) |]
+    claims
+    |> Auth.generateJWT (secret, SecurityAlgorithms.HmacSha256) issuer (DateTime.UtcNow.AddHours(1.0))
+
+let handlePostToken =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! model = ctx.BindJsonAsync<LoginViewModel>()
+
+            // authenticate user
+
+            let token = generateToken model.Email
+
+            return! json {Token = token} next ctx
+}
 let webApp = router {
+    post "/token" handlePostToken
+
     forward "/api" (subscriptionsRouter eventStore)
     forward "/smapi" (smapiRouter eventStore)
 }
@@ -25,6 +70,7 @@ let configureSerialization (services:IServiceCollection) =
     services.AddSingleton<IJsonSerializer>(NewtonsoftJsonSerializer fableJsonSettings)
 
 let app = application {
+    use_jwt_authentication secret issuer
     url ("http://0.0.0.0:" + port.ToString() + "/")
     use_router webApp
     memory_cache
@@ -33,4 +79,7 @@ let app = application {
     use_gzip
 }
 
-run app
+[<EntryPoint>]
+let main _ =
+    run app
+    0
