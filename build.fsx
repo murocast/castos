36abf89816320199d4f3dbc55fe8dcc5cb886a77
brase <bastibrandt@posteo.de>
2020-10-14 21:36:16 +0200

    removed comments

--- build.fsx
+++ build.fsx
@@ -56,20 +56,6 @@ let runTool cmd args workingDir =
     |> Proc.run
     |> ignore
 
-// let runDotNet cmd workingDir =
-//     let result =
-//         DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-//     if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
-
-// let openBrowser url =
-//     //https://github.com/dotnet/corefx/issues/10361
-//     Command.ShellCommand url
-//     |> CreateProcess.fromCommand
-//     |> CreateProcess.ensureExitCodeWithMessage "opening browser failed"
-//     |> Proc.run
-//     |> ignore
-
-
 Target.create "Clean" (fun _ ->
     [ deployDir
       clientDeployPath ]
@@ -82,42 +68,6 @@ Target.create "Bundle" (fun _ ->
     dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
     npm "run build" "."
 )
-
-// Target.create "Build" (fun _ ->
-//     runDotNet "build" serverPath
-//     Shell.regexReplaceInFileWithEncoding
-//         "let app = \".+\""
-//        ("let app = \"" + release.NugetVersion + "\"")
-//         System.Text.Encoding.UTF8
-//         (Path.combine clientPath "Version.fs")
-//     runTool yarnTool "webpack-cli -p" __SOURCE_DIRECTORY__
-// )
-
-// Target.create "Run" (fun _ ->
-//     let server = async {
-//         runDotNet "watch run" serverPath
-//     }
-//     let client = async {
-//         runTool yarnTool "webpack-dev-server" __SOURCE_DIRECTORY__
-//     }
-//     let browser = async {
-//         do! Async.Sleep 5000
-//         openBrowser "http://localhost:8080"
-//     }
-
-//     let vsCodeSession = Environment.hasEnvironVar "vsCodeSession"
-//     let safeClientOnly = Environment.hasEnvironVar "safeClientOnly"
-
-//     let tasks =
-//         [ if not safeClientOnly then yield server
-//           yield client
-//           if not vsCodeSession then yield browser ]
-
-//     tasks
-//     |> Async.Parallel
-//     |> Async.RunSynchronously
-//     |> ignore
-// )
 
 Target.create "Run" (fun _ ->
     dotnet "build" sharedPath
